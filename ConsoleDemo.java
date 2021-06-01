3dd605bee2a5fdf17820fcf24a79f897409d0547
bhagyashri-nimkarde <85113991+bhagyashri-nimkarde@users.noreply.github.com>
2021-06-01 17:16:45 +0530

    Moved to method (#1)

--- ConsoleDemo.java
+++ ConsoleDemo.java
@@ -1,6 +1,11 @@
 public class ConsoleDemo
 {
 	public static void main(String[] args) {
+		print();
+		
+	}
+	
+	private static void print() {
 		System.out.println("This your first git project");
 	}
 }
